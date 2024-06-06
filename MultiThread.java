71161301cebee4cc9899a188ef5b9290659ca804
sujiuzheng1 <sujiuzheng1@jd.com>
2024-06-06 17:46:22 +0800

    修改列表变量名

--- MultiThread.java
+++ MultiThread.java
@@ -7,9 +7,9 @@ public class MultiThread {
         // 获取Java线程管理MXBean
         ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
         // 不需要获取同步的monitor和synchronizer信息，仅获取线程和线程堆栈信息
-        ThreadInfo[] threadInfos = threadMXBean.dumpAllThreads(false, false);
+        ThreadInfo[] threadInfoList = threadMXBean.dumpAllThreads(false, false);
         // 遍历线程信息，仅打印线程ID和线程名称信息
-        for (ThreadInfo threadInfo : threadInfos) {
+        for (ThreadInfo threadInfo : threadInfoList) {
             System.out.println("[" + threadInfo.getThreadId() + "] " + threadInfo.getThreadName());
             System.out.println(threadInfo);
         }
